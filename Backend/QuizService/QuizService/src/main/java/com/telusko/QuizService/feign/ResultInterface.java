cd1de3b996d6b7f35c6b049bac789dad8ede7839
Kusum Bagdawat <bagdawatkusum@gmail.com>
2026-07-19 11:48:04 +0530

    Update ResultInterface.java

--- Backend/QuizService/QuizService/src/main/java/com/telusko/QuizService/feign/ResultInterface.java
+++ Backend/QuizService/QuizService/src/main/java/com/telusko/QuizService/feign/ResultInterface.java
@@ -5,7 +5,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient("RESULT-SERVICE")
+@FeignClient(
+    name = "result-service",
+    url = "${result.service.url}"
+)
 public interface ResultInterface {
 
     @PostMapping("/result/save")
